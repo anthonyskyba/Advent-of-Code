476570acb13e88f87fcd407654e79ce1c123327a
Anthony Skyba <122836976+anthonyskyba@users.noreply.github.com>
2023-09-24 09:16:04 -0400

    Create part_1.js

---
+++ 2015/10/part_1.js
@@ -0,0 +1,24 @@
+let input = "1113122113 "
+let output = ""
+let index = -1
+let lastDigit
+let amount = "0"
+
+for (let i = 0; i < 40; i++) {
+    for (let digit of input) {
+        index++
+        if (index == 0) lastDigit = digit
+        if (index + 1 == input.length || digit != lastDigit) { 
+            output += amount + lastDigit
+            amount = "1"
+        }
+        else amount = String(Number(amount) + 1)
+        lastDigit = digit
+    }
+    input = output + " "
+    output = ""
+    index = -1
+    amount = "0"
+}
+
+console.log(input.length - 1)
