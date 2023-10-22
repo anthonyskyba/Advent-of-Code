7f6b6c813599277291eaadd4579a447b1e71f3cc
Anthony Skyba <122836976+anthonyskyba@users.noreply.github.com>
2023-10-22 13:20:42 -0400

    Create part_2.js

---
+++ 2015/13/part_2.js
@@ -0,0 +1,55 @@
+let input = " Alice would gain 54 happiness units by sitting next to Bob. Alice would lose 81 happiness units by sitting next to Carol. Alice would lose 42 happiness units by sitting next to David. Alice would gain 89 happiness units by sitting next to Eric. Alice would lose 89 happiness units by sitting next to Frank. Alice would gain 97 happiness units by sitting next to George. Alice would lose 94 happiness units by sitting next to Mallory. Bob would gain 3 happiness units by sitting next to Alice. Bob would lose 70 happiness units by sitting next to Carol. Bob would lose 31 happiness units by sitting next to David. Bob would gain 72 happiness units by sitting next to Eric. Bob would lose 25 happiness units by sitting next to Frank. Bob would lose 95 happiness units by sitting next to George. Bob would gain 11 happiness units by sitting next to Mallory. Carol would lose 83 happiness units by sitting next to Alice. Carol would gain 8 happiness units by sitting next to Bob. Carol would gain 35 happiness units by sitting next to David. Carol would gain 10 happiness units by sitting next to Eric. Carol would gain 61 happiness units by sitting next to Frank. Carol would gain 10 happiness units by sitting next to George. Carol would gain 29 happiness units by sitting next to Mallory. David would gain 67 happiness units by sitting next to Alice. David would gain 25 happiness units by sitting next to Bob. David would gain 48 happiness units by sitting next to Carol. David would lose 65 happiness units by sitting next to Eric. David would gain 8 happiness units by sitting next to Frank. David would gain 84 happiness units by sitting next to George. David would gain 9 happiness units by sitting next to Mallory. Eric would lose 51 happiness units by sitting next to Alice. Eric would lose 39 happiness units by sitting next to Bob. Eric would gain 84 happiness units by sitting next to Carol. Eric would lose 98 happiness units by sitting next to David. Eric would lose 20 happiness units by sitting next to Frank. Eric would lose 6 happiness units by sitting next to George. Eric would gain 60 happiness units by sitting next to Mallory. Frank would gain 51 happiness units by sitting next to Alice. Frank would gain 79 happiness units by sitting next to Bob. Frank would gain 88 happiness units by sitting next to Carol. Frank would gain 33 happiness units by sitting next to David. Frank would gain 43 happiness units by sitting next to Eric. Frank would gain 77 happiness units by sitting next to George. Frank would lose 3 happiness units by sitting next to Mallory. George would lose 14 happiness units by sitting next to Alice. George would lose 12 happiness units by sitting next to Bob. George would lose 52 happiness units by sitting next to Carol. George would gain 14 happiness units by sitting next to David. George would lose 62 happiness units by sitting next to Eric. George would lose 18 happiness units by sitting next to Frank. George would lose 17 happiness units by sitting next to Mallory. Mallory would lose 36 happiness units by sitting next to Alice. Mallory would gain 76 happiness units by sitting next to Bob. Mallory would lose 34 happiness units by sitting next to Carol. Mallory would gain 37 happiness units by sitting next to David. Mallory would gain 40 happiness units by sitting next to Eric. Mallory would gain 18 happiness units by sitting next to Frank. Mallory would gain 7 happiness units by sitting next to George"
+let happinessValues = {}
+let uniqueNames = ["Santa"]
+
+for (let rule of input.split(".")) {
+    rule = rule.split(" ")
+    rule.shift()
+
+    let symbol = ""
+    if (rule[2] == "lose") symbol = "-"
+
+    happinessValues[rule[0] + rule[10]] = Number(symbol + rule[3])
+    if (!uniqueNames.includes(rule[0])) uniqueNames.push(rule[0])
+}
+
+function allPossibleOutcomes(...input) {
+    if (input.length == 1)
+        return [[input[0]]]
+
+    let allPaths = []
+    let values = input.slice(0)
+    for (let value of values) {
+        values = input.slice(0)
+        values.splice(values.indexOf(value), 1)
+
+        let outcomes = allPossibleOutcomes(...values)
+        for (let i = 0; i < outcomes.length; i++) {
+            allPaths.push([value].concat(outcomes[i]))
+        }            
+    }
+    return allPaths
+}
+
+let mostHappiness = 0
+let happiness = 0
+let counter = 0
+
+for (let variation of allPossibleOutcomes(...uniqueNames)) {
+    for (let i = 1; i < variation.length; i++) {
+        if (variation[i] == "Santa" || variation[i - 1] == "Santa") continue
+
+        happiness += happinessValues[`${variation[i - 1] + variation[i]}`]
+        happiness += happinessValues[`${variation[i] + variation[i - 1]}`]
+    }
+
+    if (variation[0] != "Santa" && variation[8] != "Santa") {
+        happiness += happinessValues[`${variation[0] + variation[8]}`]
+        happiness += happinessValues[`${variation[8] + variation[0]}`]
+    }
+
+    if (happiness > mostHappiness) mostHappiness = happiness
+    happiness = 0
+}
+
+console.log(mostHappiness)
