0623ca53bcf2292b72af99149b461d964659cb41
Anthony Skyba <122836976+anthonyskyba@users.noreply.github.com>
2023-12-02 20:00:42 -0500

    Update part_1.js

--- 2015/15/part_1.js
+++ 2015/15/part_1.js
@@ -1,34 +1,42 @@
-let input = [
-    "Sugar: capacity 3, durability 0, flavor 0, texture -3, calories 2",
-    "Sprinkles: capacity -3, durability 3, flavor 0, texture 0, calories 9",
-    "Candy: capacity -1, durability 0, flavor 4, texture 0, calories 1",
-    "Chocolate: capacity 0, durability 0, flavor -2, texture 2, calories 8",
-]
-// this entire thing is hard coded
-
 let teaspoons = [[0, 0, 0, 0]]
+let values = [0, 0, 0, 0]
+let previousValue
+
+function increment(array) {
+    let arraySum = array.reduce((a, b) => a + b)
+    if (arraySum <= 100)
+        array[3] = 100 - (array[0] + array[1] + array[2])
+        if (array[3] < 0) array[3] = 100
+    else if (arraySum > 100)
+        array[3] = 100
+    if (previousValue == array[3]) array[3] = array[3] + 1
+    previousValue = array[3]
+    return array
+}
 
 while (teaspoons[0][0] != 100) {
-    let values = teaspoons[0].concat()
-
     if (values[3] == 100) {
         values[3] = 0
         if (values[2] != 100)
             values[2] = values[2] + 1
-        else
+        else {
+            values[2] = 0
             if (values[1] != 100)
                 values[1] = values[1] + 1
-            else
-                if (values[0] != 100)
-                    values[0] = values[0] + 1
+            else {
+                values[1] = 0
+                values[0] = values[0] + 1
+            }
+        }
     } else {
-        values[3] = values[3] + 1
+        values = increment(values)
     }
-    teaspoons.unshift(values)
+    if (values.reduce((a, b) => a + b) == 100) {
+        teaspoons.unshift(values.concat())
+    }
 }
 
 function score(val1, val2, val3, val4) {
-    console.log(val1, val2, val3, val4, "values")
     let capacity = val1 * 3 + val2 * -3 + val3 * -1 + val4 * 0
     let durability = val1 * 0 + val2 * 3 + val3 * 0 + val4 * 0
     let flavor = val1 * 0 + val2 * 0 + val3 * 4 + val4 * -2
@@ -46,17 +54,13 @@ function score(val1, val2, val3, val4) {
     if (texture < 0) texture = 0
     else if (texture == 0) texture = 1
 
-    console.log(capacity, durability, flavor, texture, "vals")
-    console.log(capacity * durability * flavor * texture, "output")
     return capacity * durability * flavor * texture
 }
 
 let bestScore = -1
 for (let combination of teaspoons) {
-    if (combination.reduce((a, b) => a + b) == 100) {
-        let score2 = score(...combination)
-        if (score2 > bestScore) bestScore = score2
-    }
+    let score2 = score(...combination)
+    if (score2 > bestScore) bestScore = score2
 }
 
 console.log(bestScore)
