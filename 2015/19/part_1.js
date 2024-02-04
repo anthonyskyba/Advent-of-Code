e0ade8f57433f9cb03bb111c909615b439cf4b4d
Anthony Skyba <122836976+anthonyskyba@users.noreply.github.com>
2024-02-04 15:42:37 -0500

    Update part_1.js

--- 2015/19/part_1.js
+++ 2015/19/part_1.js
@@ -1,43 +1,28 @@
 let rules = "Al => ThF Al => ThRnFAr B => BCa B => TiB B => TiRnFAr Ca => CaCa Ca => PB Ca => PRnFAr Ca => SiRnFYFAr Ca => SiRnMgAr Ca => SiTh F => CaF F => PMg F => SiAl H => CRnAlAr H => CRnFYFYFAr H => CRnFYMgAr H => CRnMgYFAr H => HCa H => NRnFYFAr H => NRnMgAr H => NTh H => OB H => ORnFAr Mg => BF Mg => TiMg N => CRnFAr N => HSi O => CRnFYFAr O => CRnMgAr O => HP O => NRnFAr O => OTi P => CaP P => PTi P => SiRnFAr Si => CaSi Th => ThCa Ti => BP Ti => TiTi e => HF e => NAl e => OMg";
 let medicine = "CRnSiRnCaPTiMgYCaPTiRnFArSiThFArCaSiThSiThPBCaCaSiRnSiRnTiTiMgArPBCaPMgYPTiRnFArFArCaSiRnBPMgArPRnCaPTiRnFArCaSiThCaCaFArPBCaCaPTiTiRnFArCaSiRnSiAlYSiThRnFArArCaSiRnBFArCaCaSiRnSiThCaCaCaFYCaPTiBCaSiThCaSiThPMgArSiRnCaPBFYCaCaFArCaCaCaCaSiThCaSiRnPRnFArPBSiThPRnFArSiRnMgArCaFYFArCaSiRnSiAlArTiTiTiTiTiTiTiRnPMgArPTiTiTiBSiRnSiAlArTiTiRnPMgArCaFYBPBPTiRnSiRnMgArSiThCaFArCaSiThFArPRnFArCaSiRnTiBSiThSiRnSiAlYCaFArPRnFArSiThCaFArCaCaSiThCaCaCaSiRnPRnCaFArFYPMgArCaPBCaPBSiRnFYPBCaFArCaSiAl";
-let used = [];
-let obj = {};
+let used = new Set();
 
 rules = rules.replace(/\s=>\s/g, "=").split(" ");
 
 for (let rule of rules) {
     rule = rule.split("=");
+    let combination = rule[0];
+    let change = rule[1];
 
-    if (obj[rule[0]] == undefined)
-        obj[rule[0]] = rule[1];
-    else
-        obj[rule[0]] = obj[rule[0]] + " " + rule[1];
-}
-
-rules = obj;
-
-function check(combination, change) {
-    let re = new RegExp(combination, "i");
-    let distinctModule = medicine.replace(re, change);
-
-    if (!used.includes(distinctModule)) used.push(distinctModule);
-}
-
-for (let i = 0; i < medicine.length; i++) {
-    let character = medicine[i];
-
-    if (character = "e") {
-        for (let change of rules["e"].split(" "))
-            check("e", change);
-    } else if (character == character.toLowerCase()) {
-        continue
-    } else {
-        if (!(character == "B" || character == "F" || character == "H" || character == "N" || character == "O" || character == "P"))
-            character = character + medicine[i + 1];
-
-        for (let change of rules[character].split(" "))
-            check(character, change);
+    let removedMedicine = "";
+    let re = new RegExp(combination);
+    
+    while (true) {
+        let slicedMedicine = medicine.slice(removedMedicine.length);
+        
+        let match = re.exec(slicedMedicine);
+        if (!match) break;
+        
+        let distinctModule = removedMedicine + slicedMedicine.replace(re, change);
+        used.add(distinctModule);
+        
+        removedMedicine += slicedMedicine.slice(0, match.index + combination.length);
     }
 }
 
-console.log(used.length);
+console.log(used.size);
